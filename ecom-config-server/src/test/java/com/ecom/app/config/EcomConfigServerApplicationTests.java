63600deae9a78014d5ce91710dafda7727bc4c10
pragnani <pragnani@gmail.com>
2017-01-28 23:38:58 +0530

    Added config Server for Env specific properties

---
+++ ecom-config-server/src/test/java/com/ecom/app/config/EcomConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ecom.app.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcomConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
