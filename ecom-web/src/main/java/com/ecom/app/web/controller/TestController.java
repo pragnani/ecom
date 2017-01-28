28472af8b029a7093916ba659f13dc300272dbf0
pragnani <pragnani@gmail.com>
2017-01-29 02:23:42 +0530

    Environment Specific Configuration and Cloud integration

--- ecom-web/src/main/java/com/ecom/app/web/controller/TestController.java
+++ ecom-web/src/main/java/com/ecom/app/web/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.ecom.app.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RefreshScope
 @Controller
 public class TestController {
+	
+	
+	@Value("${application.env.name}")
+	private String applicationEnvName;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public @ResponseBody String getHelloWorld() {
-		return "Hello world";
+		return "Hello world "+ applicationEnvName;
 	}
 }
