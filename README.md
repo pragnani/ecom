c385d3c594dcedd878dfbfa4da4f06a2d04223a1
pragnani <pragnani@gmail.com>
2017-01-29 14:30:43 +0530

    Added cloud configuration to Services

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ecom
+
+#----------------How to RUN------#
+
+1. In order to start a web or service layer , we need to first start config server. 
+
+ecom-config-server-> Run as Spring boot application or from command prompt
+
+java -jar ecom-config-server\target\jar file version
+
+To run web application we need supply environment we are currently working as a argument to the 
+server
+
+To do this Right click on project -> run as-> run configuration ->spring boot application->
+go to Arguments Tag->
+Vm Arguments add the following argument
+
+-Dspring.profiles.active=dev
--- ecom-services/src/main/java/com/ecom/app/services/rest/controller/TestController.java
+++ ecom-services/src/main/java/com/ecom/app/services/rest/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.ecom.app.services.rest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class TestController {
+
+	@Value("${application.env.name}")
+	private String applicationEnvName;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getHelloWorld() {
-		return "Hello world";
+		return "Hello world  "+applicationEnvName;
 	}
 }
